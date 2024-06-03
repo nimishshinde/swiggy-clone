a673c05b2a7977918b22c845148f3384ca03ccc2
nimish shinde <shinde.nimish3001@gmail.com>
2024-06-03 20:40:59 +0530

    Initial setup, Parcel setup.

---
+++ app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <div>Hi Hahaha</div>;
+}
+
+const ROOT = ReactDOM.createRoot(document.getElementById("root"));
+ROOT.render(<App />);
